d09790a6810a1c27884f37ccb8217127a6a74fcf
Sendhil Panchadsaram <sendhilp@fb.com>
2021-04-15 00:36:12 -0700

    Fixing bug with toggling of trackpad.
    Wasn't actually refreshing properly. Had to rely on an Apple Script that
    I found here - https://itectec.com/askdifferent/macos-toggle-natural-scrolling-from-command-line-with-reload/

--- scripts/switch-mouse-profile.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import subprocess
-
-if "1" in subprocess.run(["defaults", "read", "NSGlobalDomain", "com.apple.swipescrolldirection"], stdout=subprocess.PIPE).stdout.decode('utf-8'):
-    subprocess.run(["defaults", "write", "NSGlobalDomain", "com.apple.swipescrolldirection", "-bool", "NO"])
-else:
-    subprocess.run(["defaults", "write", "NSGlobalDomain", "com.apple.swipescrolldirection", "-bool", "YES"])
