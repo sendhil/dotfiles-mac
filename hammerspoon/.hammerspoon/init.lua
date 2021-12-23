18667156e811ce56ebf22900ea5feda278b3ea9a
Sendhil Panchadsaram <sendhilp@gmail.com>
2021-12-23 14:59:06 -0800

    Fixing hammerspoon cli install.

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -2,6 +2,8 @@ require("hs.ipc")
 stackline = require "stackline.stackline.stackline"
 stackline:init()
 
+hs.ipc.cliInstall("/opt/homebrew")
+
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
   hs.reload()
 end)
