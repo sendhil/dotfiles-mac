4fea76cb645c11f9cdf52accba9567e5540f7ccf
Sendhil Panchadsaram <sendhilp@gmail.com>
2022-09-21 00:17:25 -0700

    Adjusting `timer` to display message across all screens.

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -5,6 +5,13 @@ stackline:init()
 
 hs.ipc.cliInstall("/opt/homebrew")
 
+function displayMessageOnAllScreens(msg, duration)
+  screens = hs.screen.allScreens()
+  for key,val in pairs(screens) do
+    hs.alert.show(msg, val, duration)
+  end
+end
+
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
   hs.reload()
 end)
