f1cf3a7d7ae27850452ba6fd670f41a8ee379bdc
Sendhil Panchadsaram <sendhilp@gmail.com>
2022-10-06 23:12:57 -0700

    scripts: Adjusting script to swap between keyboard and mouse profile.

--- scripts/switch-mouse-and-keyboard-profile.py
+++ scripts/switch-mouse-and-keyboard-profile.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 
 import subprocess
+import pathlib
+import os
 
 CLI_PATH = "/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli"
 
 BUILT_IN_PROFILE = "BuiltInKeyboard"
 EXTERNAL_PROFILE = "ExternalKeyboard"
+    
+TRACKPAD_SCRIPT_PATH = os.path.join(pathlib.Path(__file__).parent.resolve(), 'toggle-trackpad.sh')
 
 if BUILT_IN_PROFILE in subprocess.run([CLI_PATH, "--show-current-profile-name"], stdout=subprocess.PIPE).stdout.decode('utf-8'):
     subprocess.run([CLI_PATH, "--select-profile", EXTERNAL_PROFILE])
-    # subprocess.run(['./toggle-trackpad.sh'], stdout=subprocess.PIPE)
-    # subprocess.run(["defaults", "write", "NSGlobalDomain", "com.apple.swipescrolldirection", "-bool", "NO"])
+    subprocess.run([TRACKPAD_SCRIPT_PATH], stdout=subprocess.PIPE)
 else:
     subprocess.run([CLI_PATH, "--select-profile", BUILT_IN_PROFILE])
-    # subprocess.run(['./toggle-trackpad.sh'], stdout=subprocess.PIPE)
-    # subprocess.run(["defaults", "write", "NSGlobalDomain", "com.apple.swipescrolldirection", "-bool", "YES"])
+    subprocess.run([TRACKPAD_SCRIPT_PATH], stdout=subprocess.PIPE)
