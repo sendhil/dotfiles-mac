c8d46667b95d7c33e3f3ced25153e950e5ee7026
Sendhil Panchadsaram <sendhilp@gmail.com>
2023-04-15 19:27:40 -0700

    Tweaking scripts to toggle trackpad in Ventura.
    Atm - just piping this script directly into Alfred as I hit some strange
    issues with calling osascript from python, will fix this at some point.

---
+++ scripts/switch-keyboard-profile.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import subprocess
+import pathlib
+import os
+
+CLI_PATH = "/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli"
+
+BUILT_IN_PROFILE = "BuiltInKeyboard"
+EXTERNAL_PROFILE = "ExternalKeyboard"
+    
+TRACKPAD_SCRIPT_PATH = os.path.join(pathlib.Path(__file__).parent.resolve(), 'toggle-trackpad.sh')
+
+if BUILT_IN_PROFILE in subprocess.run([CLI_PATH, "--show-current-profile-name"], stdout=subprocess.PIPE).stdout.decode('utf-8'):
+    subprocess.run([CLI_PATH, "--select-profile", EXTERNAL_PROFILE])
+else:
+    subprocess.run([CLI_PATH, "--select-profile", BUILT_IN_PROFILE])
