264ef029e4c2d2313f5fc5ad882f8025d79603c8
Sendhil Panchadsaram <sendhilp@gmail.com>
2025-07-27 14:35:16 -0700

    vim: more mason tweaks

--- neovim/.config/nvim/lua/plugins/mason.lua
+++ neovim/.config/nvim/lua/plugins/mason.lua
@@ -1,60 +1,26 @@
 return {
   {
     "williamboman/mason.nvim",
-    cmd = "Mason",
-    build = ":MasonUpdate",
-    event = "VeryLazy",
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-    },
-    opts = {
-      ensure_installed = {
-        -- Language servers
-        "clangd",
-        "rust-analyzer",
-        "pyright",
-        "typescript-language-server",
-        "gopls",
-        "lua-language-server",
-        "buf-language-server",
-        "yaml-language-server",
-        "nil_ls",
-        
-        -- Formatters
-        "black",
-        "gofumpt",
-        "goimports",
-        
-        -- Linters
-        "ruff",
-        "clang-tidy",
-      },
-    },
-    config = function(_, opts)
-      require("mason").setup(opts)
-      local mr = require("mason-registry")
-      mr:on("package:install:success", vim.schedule_wrap(function(pkg)
-        vim.notify(string.format('Mason: %s was successfully installed', pkg.name))
-      end))
+    lazy = false,
+    priority = 100,
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+          }
+        }
+      })
       
-      local function ensure_installed()
-        for _, tool in ipairs(opts.ensure_installed) do
-          local p = mr.get_package(tool)
-          if not p:is_installed() then
-            p:install()
-          end
-        end
-      end
-      
-      if mr.refresh then
-        mr.refresh(ensure_installed)
-      else
-        ensure_installed()
-      end
+      -- Don't auto-install in the main Mason config
+      -- Let mason-lspconfig and mason-tool-installer handle it
     end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
+    lazy = false,
     dependencies = { "williamboman/mason.nvim" },
     config = function()
       require("mason-lspconfig").setup({
@@ -67,7 +33,7 @@ return {
           "lua_ls",
           "bufls",
           "yamlls",
-          "nil_ls",
+          -- "nil_ls", -- Comment out for now, might not be available
         },
         automatic_installation = true,
       })
