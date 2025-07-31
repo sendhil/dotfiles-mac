3b40a0d560126e88a77b6098c5cb983af7cb1cbd
Sendhil Panchadsaram <spanchadsaram@anduril.com>
2025-07-31 14:26:08 -0700

    Adding some themes

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,24 @@
 return {
-    -- the colorscheme should be available when starting Neovim
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            -- load the colorscheme here
-            vim.cmd([[colorscheme tokyonight]])
-        end,
-    },
-    { "rakr/vim-one" },
-    { "shaunsingh/nord.nvim" },
-    { "navarasu/onedark.nvim" },
-    { "catppuccin/nvim" },
-    { "cpea2506/one_monokai.nvim" },
-    { "marko-cerovac/material.nvim" },
-    { "sainnhe/sonokai" },
-    { "sainnhe/everforest" },
-    { "rmehri01/onenord.nvim" },
-    { "overcache/NeoSolarized" },
+  -- the colorscheme should be available when starting Neovim
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+  { "rakr/vim-one" },
+  { "shaunsingh/nord.nvim" },
+  { "navarasu/onedark.nvim" },
+  { "catppuccin/nvim" },
+  { "cpea2506/one_monokai.nvim" },
+  { "marko-cerovac/material.nvim" },
+  { "sainnhe/sonokai" },
+  { "sainnhe/everforest" },
+  { "rmehri01/onenord.nvim" },
+  { "maxmx03/solarized.nvim" },
+  { 'projekt0n/github-nvim-theme', name = 'github-theme' },
+  { "Shatur/neovim-ayu" }
 }
