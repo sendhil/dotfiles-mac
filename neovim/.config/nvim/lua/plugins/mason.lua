155144aec788b17e60c2758b7200435ab908719c
Sendhil Panchadsaram <spanchadsaram@anduril.com>
2025-07-30 18:59:53 -0700

    Removing mason's auto config of lsp

--- neovim/.config/nvim/lua/plugins/mason.lua
+++ neovim/.config/nvim/lua/plugins/mason.lua
@@ -16,41 +16,9 @@ return {
   },
   {
     "williamboman/mason-lspconfig.nvim",
+    enabled = false, -- Disable mason-lspconfig to prevent duplicate server initialization
     event = "VeryLazy",
     dependencies = { "williamboman/mason.nvim" },
-    config = function()
-      -- Detect architecture
-      local is_arm = vim.fn.system("uname -m"):match("arm") or vim.fn.system("uname -m"):match("aarch64")
-      
-      -- Build ensure_installed list based on architecture
-      local ensure_installed = {
-        "rust_analyzer",
-        "pyright",
-        "ts_ls", 
-        "gopls",
-        "lua_ls",
-        "buf_ls",
-        "yamlls",
-        -- "nil_ls", -- Comment out for now, might not be available
-      }
-      
-      -- Only add clangd on non-ARM systems
-      if not is_arm then
-        table.insert(ensure_installed, 1, "clangd")
-      end
-      
-      require("mason-lspconfig").setup({
-        ensure_installed = ensure_installed,
-        automatic_installation = false, -- Don't auto-install
-        -- This handles the mapping between lspconfig names and mason names
-        handlers = {
-          -- Default handler
-          function(server_name)
-            require("lspconfig")[server_name].setup({})
-          end,
-        },
-      })
-    end,
   },
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
